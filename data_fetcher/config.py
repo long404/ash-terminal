64388cd9542e6a2deaa300795fc51f5f833d696d
long404 <long404@gmail.com>
2025-05-02 09:17:47 +0300

    Added BASE_URL and TABLE_NAME and changed the constant name for the API key to ALPHA_API_KEY

--- data_fetcher/config.py
+++ data_fetcher/config.py
@@ -1,3 +1,5 @@
-API_KEY = 'H2DOHSBNPLK08NMA'
+BASE_URL = "https://www.alphavantage.co/query"
+ALPHA_API_KEY = 'H2DOHSBNPLK08NMA'
 SYMBOLS = ['TQQQ', 'QQQ', 'SPY', 'DIA']
 DATABASE_DIR = '../databases'
+TABLE_NAME = 'intraday_data'
