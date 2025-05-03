1ab6123b7448d2e5a331dbe654c54ba7e52d134d
long404 <long404@gmail.com>
2025-05-03 21:12:47 +0300

    Removed the NO_SYMBOL constant - no longer needed in the fetch code.

--- data_fetcher/config.py
+++ data_fetcher/config.py
@@ -3,5 +3,4 @@ ALPHA_API_KEY = 'H2DOHSBNPLK08NMA'
 SYMBOLS = ['TQQQ', 'QQQ', 'SPY', 'DIA']
 DATABASE_DIR = '/Users/alex.popov/develop/finance/ash-terminal/databases'
 TABLE_NAME = 'intraday_data'
-NO_SYMBOL = '404404'
 DEBUG = 1
