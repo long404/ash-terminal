3ced245c7177cb5122a751f8db7e52a002e5b994
long404 <long404@gmail.com>
2025-05-05 19:03:00 +0300

    Added logging

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,32 @@
 from flask import Flask, request, jsonify
 import db_access
+import logging
+import config
+import sys
 
+# setup logging
+log = logging.getLogger(f"ash-be")
+log_level = getattr(logging, config.LOG_LEVEL.upper(), logging.INFO)
+log_format = "%(asctime)s [%(levelname)s] [%(name)s] [%(funcName)s] %(message)s"
+if config.LOG_TO_FILE:
+    logging.basicConfig(
+        level=log_level,
+        format=log_format,
+        filename=config.LOG_FILE_PATH,
+        filemode='a'
+    )
+else:
+    logging.basicConfig(
+        level=log_level,
+        format=log_format
+    )
+
+def logcritical_and_exit(err):
+    log.critical(err)
+    sys.exit(-1)
+
+
+# =========== Backend ===========
 app = Flask(__name__)
 
 @app.route('/api/history')
